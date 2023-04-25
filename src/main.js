4b1e863ade93c6acf679caf3020cae7bfb6efab7
fvan-wij <fvan-wij@student.codam.nl>
2023-04-25 10:43:31 +0200

    Looping now works, not integrated yet with checkbox though

--- src/main.js
+++ src/main.js
@@ -1,27 +1,57 @@
-const	synthA = new Tone.FMSynth().toMaster();
-let steps = document.querySelectorAll(".step");
-let playerState = 0;
+// const	synthA = new Tone.FMSynth().toMaster();
+// let steps = document.querySelectorAll(".step");
+// let playerState = 0;
+//
+// const changePlayerstate = document.getElementById("play");
+// changePlayerstate.addEventListener("click", () =>
+// {
+// 	playerState++;
+// 	if (playerState == 1)
+// 	{
+// 		steps.forEach(steps => 
+// 		{
+// 			if (steps.checked)
+// 			{
+// 				console.log("Player has started.");
+// 				synthA.triggerAttackRelease("C2", "16n");
+// 			}
+// 		});
+//
+// 	}
+// 	else if (playerState == 2)
+// 		console.log("Player is paused.");
+// 	if (playerState > 1)
+// 		playerState = 0;
+// });
+//
 
-const changePlayerstate = document.getElementById("play");
-changePlayerstate.addEventListener("click", () =>
+// create two monophonic synths
+const synthA = new Tone.FMSynth().toDestination();
+const synthB = new Tone.AMSynth().toDestination();
+//play a note every quarter-note
+const loopA = new Tone.Loop(time => {
+	synthA.triggerAttackRelease("C2", "8n", time);
+}, "4n").start(0);
+//play another note every off quarter-note, by starting it "8n"
+const loopB = new Tone.Loop(time => {
+	synthB.triggerAttackRelease("C4", "8n", time);
+}, "4n").start("8n");
+
+
+const start = document.getElementById('play');
+start.addEventListener( 'click', async () =>
 {
-	playerState++;
-	if (playerState == 1)
-	{
-		steps.forEach(steps => 
-		{
-			if (steps.checked)
-			{
-				console.log("Player has started.");
-				synthA.triggerAttackRelease("C2", "16n");
-			}
-		});
+await Tone.start();
+console.log("Audio is ready");
+Tone.Transport.start();
+Tone.Transport.bpm.rampTo(120);
+});
 
-	}
-	else if (playerState == 2)
-		console.log("Player is paused.");
-	if (playerState > 1)
-		playerState = 0;
+const stop = document.getElementById('stop');
+stop.addEventListener( 'click', async () =>
+{
+console.log("Audio has stopped.");
+Tone.Transport.stop();
 });
 
 
