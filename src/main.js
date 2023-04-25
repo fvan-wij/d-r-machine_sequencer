0a781cc4150571a0251e47721d6842846bf09d5e
fvan-wij <fvan-wij@student.codam.nl>
2023-04-25 22:12:50 +0200

    Multiple sounds now play at the same time, but steps aren't being differentiated

--- src/main.js
+++ src/main.js
@@ -1,57 +1,56 @@
-// const	synthA = new Tone.FMSynth().toMaster();
-// let steps = document.querySelectorAll(".step");
-// let playerState = 0;
-//
-// const changePlayerstate = document.getElementById("play");
-// changePlayerstate.addEventListener("click", () =>
-// {
-// 	playerState++;
-// 	if (playerState == 1)
-// 	{
-// 		steps.forEach(steps => 
-// 		{
-// 			if (steps.checked)
-// 			{
-// 				console.log("Player has started.");
-// 				synthA.triggerAttackRelease("C2", "16n");
-// 			}
-// 		});
-//
-// 	}
-// 	else if (playerState == 2)
-// 		console.log("Player is paused.");
-// 	if (playerState > 1)
-// 		playerState = 0;
-// });
-//
+const	synthA = new Tone.FMSynth().toMaster();
+let	synths = [];
+// Initialize 8 synths, 1 synth for each step.
+const initSynths = (n) =>
+{
+	for (let i= 0; i < n; i++)
+	{
+		let synth = new Tone.FMSynth().toDestination();
+		synths.push(synth);
+	}
+	return synths;
+};
 
-// create two monophonic synths
-const synthA = new Tone.FMSynth().toDestination();
-const synthB = new Tone.AMSynth().toDestination();
-//play a note every quarter-note
-const loopA = new Tone.Loop(time => {
-	synthA.triggerAttackRelease("C2", "8n", time);
-}, "4n").start(0);
-//play another note every off quarter-note, by starting it "8n"
-const loopB = new Tone.Loop(time => {
-	synthB.triggerAttackRelease("C4", "8n", time);
-}, "4n").start("8n");
+let steps = document.querySelectorAll(".step");
 
+synths = initSynths(8);
 
+const playStepsInLoop = () =>
+{
+	const repeat = (time) =>
+	{
+		let index = 0;
+		steps.forEach((steps, index) => 
+		{
+			let synth = synths[index];
+			if (steps.checked)
+			{
+				synth.triggerAttackRelease("C3", "32n", time);
+			}
+		});
+	    // beat = (beat + 1) % 8;
+	}
+	Tone.Transport.bpm.value = 120;
+	Tone.Transport.scheduleRepeat(repeat, "8n");
+};
+
+// Start button -> starts the loop
 const start = document.getElementById('play');
 start.addEventListener( 'click', async () =>
 {
-await Tone.start();
-console.log("Audio is ready");
-Tone.Transport.start();
-Tone.Transport.bpm.rampTo(120);
+	await Tone.start();
+	console.log("Audio is ready");
+	Tone.Transport.start();
+	playStepsInLoop();
+	// Tone.Transport.bpm.rampTo(120);
 });
 
+// Stop button -> stops the loop
 const stop = document.getElementById('stop');
 stop.addEventListener( 'click', async () =>
 {
-console.log("Audio has stopped.");
-Tone.Transport.stop();
+	console.log("Audio has stopped.");
+	Tone.Transport.stop();
 });
 
 
