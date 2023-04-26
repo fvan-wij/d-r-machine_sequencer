f44faabe7db033944b9fbcc4c3f57f89676b58ec
Flip <flip@pop-os.localdomain>
2023-04-26 23:06:53 +0200

    Improved CSS styling, added kick, hats, snare and clap samples, added BPM slider.

---
+++ pythonserver_readme.txt
@@ -0,0 +1,11 @@
+1 Install Python:
+	sudo apt-get install python3.11
+
+2 Check if properly installed by using version command:
+	python3 --version
+
+3 Start server:
+	python3 -m http.server
+
+4 Go to localhost:8000
+	boem
--- src/sequencer.js
+++ src/sequencer.js
@@ -1,44 +1,212 @@
-let steps = [];
+//Master volume 
+var vol = new Tone.Volume(-12);
+
+//Initializing synth and drum samples
 const synth = new Tone.Synth().toDestination();
+const kick = new Tone.Player('../data/samples/kick.wav').toDestination();
+const oh = new Tone.Player('../data/samples/oh.wav').toDestination();
+const ch = new Tone.Player('../data/samples/ch.wav').toDestination();
+const snare	= new Tone.Player('../data/samples/snare.wav').toDestination();
+const clap = new Tone.Player('../data/samples/clap.wav').toDestination();
 
-const htmlSteps = document.querySelectorAll('.synth_step');
-const fillSteps = () => 
+//HTML QuerySelectors
+const synthSteps = document.querySelectorAll('.synth_step');
+const kickSteps = document.querySelectorAll('.kick_step');
+const ohSteps = document.querySelectorAll('.oh_step');
+const chSteps = document.querySelectorAll('.ch_step');
+const snareSteps = document.querySelectorAll('.snare_step');
+const clapSteps = document.querySelectorAll('.clap_step');
+var checkboxes = document.querySelectorAll('input');
+
+//HTML GetElementById
+const stop = document.getElementById('stop');
+const start = document.getElementById('play');
+var bpmSlider = document.getElementById('bpmRange');
+var volumeSlider = document.getElementById('bpmRange');
+var driveSlider = document.getElementById('bpmRange');
+var reverbSlider = document.getElementById('bpmRange');
+var chorusSlider = document.getElementById('bpmRange');
+
+//Global variables
+let synthStepArray = [];
+let kickStepArray = [];
+let ohStepArray = [];
+let chStepArray = [];
+let snareStepArray = [];
+let clapStepArray = [];
+
+//Fills the synthStepArray for each checkbox that is ticked
+const fillSynthSteps = () => 
 {
-	steps = [];
+	synthStepArray = [];
 	let note = 'F4';
-	console.log("checkSteps activated");
-	htmlSteps.forEach(htmlSteps => {
+	synthSteps.forEach(synthSteps => {
 
-		if (htmlSteps.checked)
-			steps.push(note);
+		if (synthSteps.checked)
+			synthStepArray.push(note);
 		else
-			steps.push(null);
+			synthStepArray.push(null);
 	});
-	return steps;
+	return synthStepArray;
 };
 
-var checkboxes = document.querySelectorAll('.synth_step');
-checkboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('change', function() {
-   		steps = fillSteps(); 
-	  	sequenceNotes();
-	  	console.log(steps);
-  })
-});
+const fillKickSteps = () => 
+{
+	kickStepArray = [];
+	let note = 'F4';
+	kickSteps.forEach(kickSteps => {
 
-const sequenceNotes = () =>
+		if (kickSteps.checked)
+			kickStepArray.push(note);
+		else
+			kickStepArray.push(null);
+	});
+	return kickStepArray;
+};
+
+const fillOhSteps = () => 
+{
+	ohStepArray = [];
+	let note = 'F4';
+	ohSteps.forEach(ohSteps => {
+
+		if (ohSteps.checked)
+			ohStepArray.push(note);
+		else
+			ohStepArray.push(null);
+	});
+	return ohStepArray;
+};
+
+const fillChSteps = () => 
+{
+	chStepArray = [];
+	let note = 'F4';
+	chSteps.forEach(chSteps => {
+
+		if (chSteps.checked)
+			chStepArray.push(note);
+		else
+			chStepArray.push(null);
+	});
+	return chStepArray;
+};
+
+const fillSnareSteps = () => 
+{
+	snareStepArray = [];
+	let note = 'F4';
+	snareSteps.forEach(snareSteps => {
+
+		if (snareSteps.checked)
+			snareStepArray.push(note);
+		else
+			snareStepArray.push(null);
+	});
+	return snareStepArray;
+};
+
+const fillClapSteps = () => 
+{
+	clapStepArray = [];
+	let note = 'F4';
+	clapSteps.forEach(clapSteps => {
+
+		if (clapSteps.checked)
+			clapStepArray.push(note);
+		else
+			clapStepArray.push(null);
+	});
+	return clapStepArray;
+};
+
+//Sequences the notes as positioned in the synthStepArray
+const sequenceSynthNotes = () =>
 {
 	const seq = new Tone.Sequence((time, note) => {
 		synth.triggerAttackRelease(note, '32n', time);
-	}, steps).start(0);
+	}, synthStepArray).start(0);
 
 };
 
-const start = document.getElementById('play');
+const sequenceKickNotes = () =>
+{
+	const seq = new Tone.Sequence((time, note) => {
+		kick.start();
+	}, kickStepArray).start(0);
+
+};
+
+const sequenceOhNotes = () =>
+{
+	const seq = new Tone.Sequence((time, note) => {
+		oh.start();
+	}, ohStepArray).start(0);
+
+};
+
+const sequenceChNotes = () =>
+{
+	const seq = new Tone.Sequence((time, note) => {
+		ch.start();
+	}, chStepArray).start(0);
+
+};
+
+const sequenceSnareNotes = () =>
+{
+	const seq = new Tone.Sequence((time, note) => {
+		snare.start();
+	}, snareStepArray).start(0);
+
+};
+
+const sequenceClapNotes = () =>
+{
+	const seq = new Tone.Sequence((time, note) => {
+		clap.start();
+	}, clapStepArray).start(0);
+
+};
+
+//Updates the sequence as soon as a checkbox change is detected
+checkboxes.forEach(function(checkbox) 
+{
+	checkbox.addEventListener('change', function() 
+	{
+
+   		synthStepArray = fillSynthSteps(); 
+	  	kickStepArray = fillKickSteps();
+	  	ohStepArray = fillOhSteps();
+	  	chStepArray = fillChSteps();
+		snareStepArray = fillSnareSteps();
+		clapStepArray = fillClapSteps();
+
+	  	sequenceSynthNotes();
+	  	sequenceKickNotes();
+	  	sequenceOhNotes();
+	  	sequenceChNotes();
+		sequenceSnareNotes();
+		sequenceClapNotes();
+	  	// console.log('synthStepArray:'+ synthStepArray + '');
+	})
+});
+
+//Sliders and settings 
+// TO DO: FIX VOLUME SLIDER AND FX SLIDERS
+bpmSlider.oninput = function() 
+{
+	Tone.Transport.bpm.value = this.value;
+}
+// volumeSlider.oninput = function() 
+// {
+// 	Tone.Destination.chain(vol);
+// }
+
+//Play or Pause the sequencer
 start.addEventListener('click', async () =>
 {
 	await Tone.start();
-	sequenceNotes();
 	if (Tone.Transport.state === 'started')
 	{
 		console.log("Paused");
@@ -46,23 +214,21 @@ start.addEventListener('click', async () =>
 	}
 	else if (Tone.Transport.state === 'paused')
 	{
-
 		console.log("Started");
 		Tone.Transport.start();
 	}
 	else if (Tone.Transport.state === 'stopped')
 	{
-
 		console.log("Started");
 		Tone.Transport.start();
 	}
-
 });
 
-const stop = document.getElementById('stop');
+//Stop the sequencer
 stop.addEventListener('click', async () =>
 {
 	if (Tone.Transport.state === 'started' || Tone.Transport.state === 'paused')
 		Tone.Transport.stop();
 	console.log("Stopped");
 });
+
